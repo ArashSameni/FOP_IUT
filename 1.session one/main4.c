181889e073b221a4f9b57fdab2c0bc7c2ad36c61
ArashSameni <74505991+ArashSameni@users.noreply.github.com>
2020-12-04 09:05:31 +0330

    Create main4.c

---
+++ 1.session one/main4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int pirhan, shalvar, joorab;
+	scanf("%d%d%d", &pirhan, &shalvar, &joorab);
+	
+	int halat_ha = pirhan*shalvar*joorab;
+	printf("%d", halat_ha);
+	return 0;
+}
