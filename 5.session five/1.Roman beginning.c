c7ff65db4a6fc73d8526646f0c38990007de06c0
ArashSameni <74505991+ArashSameni@users.noreply.github.com>
2020-12-10 05:14:55 +0330

    Create 1.Roman beginning.c

---
+++ 5.session five/1.Roman beginning.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    scanf("%d", &num);
+	switch (num)
+    {
+        case 1:
+            printf("I");
+            break;
+        case 2:
+            printf("II");
+            break;
+        case 3:
+            printf("III");
+            break;
+        case 4:
+            printf("IV");
+            break;
+        case 5:
+            printf("V");
+            break;
+        case 6:
+            printf("VI");
+            break;
+        case 7:
+            printf("VII");
+            break;
+        case 8:
+            printf("VIII");
+            break;
+        case 9:
+            printf("IX");
+            break;
+        case 10:
+            printf("X");
+            break;
+        case 11:
+            printf("XI");
+            break;
+        case 12:
+            printf("XII");
+            break;
+        default:
+            break;
+    }
+	return 0;
+}
