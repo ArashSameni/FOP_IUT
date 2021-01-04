11dd37f3cd88c79699ae2758b52cf9a849b75cfc
ArashSameni <74505991+ArashSameni@users.noreply.github.com>
2021-01-04 23:49:01 +0330

    Update NecessaryFuncs.c

--- NecessaryFuncs.c
+++ NecessaryFuncs.c
@@ -64,3 +64,21 @@ int xToDec(long int mabnaNum, int mabna){
     }
     return res;
 }
+
+int isPrime(int n){
+    if(n == 0 || n == 1)
+        return 0;
+    else if(n == 2)
+        return 1;
+    else{
+        if(n%2 == 0 || n%3 == 0)
+            return 0;
+        int i = 5;
+        while(i * i <= n){
+            if(n%i == 0 || n%(i + 2) ==0)
+                return 0;
+            i += 6;
+        }
+        return 1;
+    }
+}
